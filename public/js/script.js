d35443ab122ac2d8debac395f6998d42d40097e9
idziakjakub <idziakjakub@gmail.com>
2014-07-21 11:33:40 +0200

    siema

--- public/js/script.js
+++ public/js/script.js
@@ -3,15 +3,15 @@
 function commentsController($scope, $http)
 {
 
-$http({method: 'POST', url: 'http://staze.org/note?method=index'}).success(function(data)
-			{
-				$scope.posts = data; // response data 
-				var str = data;
-				var res = str.substring(25, str.length-5);
-				$scope.wynik = angular.fromJson(res);
-		    });
+	$http({method: 'POST', url: 'http://projekt1.com/note?method=index'}).success(function(data)
+	{
+		$scope.posts = data; // response data 
+		var str = data;
+		var res = str.substring(25, str.length-5);
+		$scope.wynik = angular.fromJson(res);
+	});
 
-$scope.submitForm = function()
+	$scope.submitForm = function()
 	{
         console.log("posting data....");
         formData = $scope.form;
@@ -19,13 +19,12 @@ $scope.submitForm = function()
     	console.log(formData.title);
 		var inde = 'index';
 
-		$http({method: 'POST', url: 'http://staze.org/note?method=add&title='+formData.title+'&content='+formData.content}).success(function(data)
-		 {
-			 console.log("wyslany ajzol....");
-			 $scope.form = "";
-		 
+		$http({method: 'POST', url: 'http://projekt1.com//note?method=add&title='+formData.title+'&content='+formData.content}).success(function(data)
+		{
+			console.log("wyslany ajzol....");
+			$scope.form = "";
 
-			 $http({method: 'POST', url: 'http://staze.org/note?method=index'}).success(function(data)
+			$http({method: 'POST', url: 'http://projekt1.com//note?method=index'}).success(function(data)
 			{
 				$scope.posts = data; // response data 
 				var str = data;
@@ -33,67 +32,16 @@ $scope.submitForm = function()
 				$scope.wynik = angular.fromJson(res);
 				console.log("odbierany ajzol....");
 		    });
-
-
-		 });
-			
-
+		 });		
     };
 
-// index : index of global DOM
-$scope.deleteComment = function(index){
-// Angular AJAX call
-$http({
-method : "GET",
-url : "index.php?action=delete&id="+$scope.comments[index].id,
-}).success(function(data){
-// Removing Data from Global DOM
-$scope.comments.splice(index,1);
-});
+	$scope.deleteComment = function(index){
+
+	$http({
+			method : "GET",
+			url : "index.php?action=delete&id="+$scope.comments[index].id,
+	}).success(function(data){
+			$scope.comments.splice(index,1);
+		});
+	}
 }
-}
-
-
-
-//var demo = angular.module('demo',[]);
-
-
-
-/*demo.controller('PostsCtrlAjax',
-
-function($scope, $http){
-
-$http({method: 'POST', url: 'http://staze.org/note?method=index'}).success(function(data)
-			{
-				$scope.posts = data; // response data 
-				var str = data;
-				var res = str.substring(25, str.length-5);
-				$scope.wynik = angular.fromJson(res);
-		    });
-
-
-
-
-
-});
-
-demo.controller('FormCtrl',function($scope, $http){
-
-		$scope.submitForm = function() {
-        console.log("posting data....");
-        formData = $scope.form;
-        console.log(formData);
-    	console.log(formData.title);
-		var inde = 'index';
-
-$http({method: 'POST', url: 'http://staze.org/note?method=add&title='+formData.title+'&content='+formData.content}).success(function(data)
-{
- console.log("wyslany ajzol....");
- });
-
-
-    };
-
-
-});
-*/
