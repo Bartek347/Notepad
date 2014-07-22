446ebb7e83da3b2b607dc1ea817362d322e0b7ed
idziakjakub <idziakjakub@gmail.com>
2014-07-22 12:58:34 +0200

    dsadas

--- public/index.php
+++ public/index.php
@@ -17,10 +17,16 @@ set_include_path(implode(PATH_SEPARATOR, array(
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
+
+
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
+
+$baseUrl = $application->baseHttp;
+    define('BASE_URL', $baseUrl);
+    
 $application->bootstrap()
             ->run();
